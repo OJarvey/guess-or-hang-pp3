e75f390cd6773970b8cf903c047c32d2d647a47c
OJarvey <165787104+OJarvey@users.noreply.github.com>
2024-08-15 12:32:57 +0100

    "Added Google Sheets API setup and data retrieval code to run.py"

---
+++ run.py
@@ -0,0 +1,19 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Constants
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Google Sheets API Setup
+CREDENTIALS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDENTIALS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('guess-or-hang')
+
+sheet1 = SHEET.worksheet('sheet1')
+data = sheet1.get_all_values()
+print(data)
